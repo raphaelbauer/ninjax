541b720375b382c47c7b133b4880f4baf1011588
Raphael A. Bauer <raphael.bauer@raphaelbauer.com>
2026-01-21 21:22:15 +0100

    resource leakage fix

--- ninja-core/src/main/java/org/ninjax/core/properties/NinjaProperties.java
+++ ninja-core/src/main/java/org/ninjax/core/properties/NinjaProperties.java
@@ -3,6 +3,7 @@ package org.ninjax.core.properties;
 import com.google.common.collect.ImmutableMap;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.Properties;
@@ -36,11 +37,12 @@ public class NinjaProperties {
         ////////////////////////////////////////////////////////////////////////
         // Load Default properties
         ////////////////////////////////////////////////////////////////////////
-        try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(DEFAULT_LOCATION_OF_APPLICATION_CONF)) {
+        try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(DEFAULT_LOCATION_OF_APPLICATION_CONF);
+             InputStreamReader inputStreamReader = new java.io.InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
             if (inputStream == null) {
                 logger.error("Sorry, unable to find " + DEFAULT_LOCATION_OF_APPLICATION_CONF);
             } else {
-                properties.load(new java.io.InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                properties.load(inputStreamReader);
             }
         } catch (IOException e) {
             logger.error("Opsi. Failure loading " + DEFAULT_LOCATION_OF_APPLICATION_CONF, e);
